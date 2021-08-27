80623579de5927235e9cf4d4ddbcf9e0b90c6bde
Ella Choy <egal21de@gmail.com>
2021-08-28 00:27:01 +0200

    array_sort

---
+++ assets/js/array.js
@@ -0,0 +1,22 @@
+console.log("lev1_1: Array sort()");
+
+let languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+  ];
+const sortierung= languages.sort();
+console.log(sortierung);
+
+console.log('');
+console.log('');
+console.log("lev1_2: Array reverse()");
