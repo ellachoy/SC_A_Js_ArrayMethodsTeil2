31111e6ace1e987d09cb1b683b1800624e946d6e
Ella Choy <egal21de@gmail.com>
2021-08-29 01:14:44 +0200

    array_join

--- assets/js/array.js
+++ assets/js/array.js
@@ -38,4 +38,45 @@ console.log(sortierung2);
 console.log('');
 console.log('');
 console.log("lev1_13: Arrays join()");
-  
+let meinText1=["Hello", "World"];
+let meinText2=["Anass", "Elaine", "Eric", "Georg"];
+let meinText3= ["Superman", "Wonderwoman", "Hulk", "Batman", "Spiderman"];
+
+{
+const join1=meinText1.join();
+const join2=meinText1.join("");
+const join3=meinText1.join(" ");
+const join4=meinText1.join("+");
+console.log(join1);
+console.log(join2);
+console.log(join3);
+console.log(join4);
+}
+
+{
+  const join1=meinText2.join();
+  const join2=meinText2.join("");
+  const join3=meinText2.join(" ");
+  const join4=meinText2.join("+");
+  console.log(join1);
+  console.log(join2);
+  console.log(join3);
+  console.log(join4);
+}
+{
+  const join1=meinText3.join();
+  const join2=meinText3.join("");
+  const join3=meinText3.join(" ");
+  const join4=meinText3.join("+");
+  console.log(join1);
+  console.log(join2);
+  console.log(join3);
+  console.log(join4);
+}
+
+
+
+
+
+
+
